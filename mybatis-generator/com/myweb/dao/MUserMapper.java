f9eb8c79a820a271fc7318423e5bc4a2cb99f033
shengbin <shengbin@DESKTOP-CBBANRT>
2017-02-03 18:59:35 +0800

    init

---
+++ mybatis-generator/com/myweb/dao/MUserMapper.java
@@ -0,0 +1,17 @@
+package com.myweb.dao;
+
+import com.myweb.model.MUser;
+
+public interface MUserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(MUser record);
+
+    int insertSelective(MUser record);
+
+    MUser selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(MUser record);
+
+    int updateByPrimaryKey(MUser record);
+}
---
+++ src/main/java/com/myweb/service/MUserServiceI.java
@@ -0,0 +1,18 @@
+package com.myweb.service;
+
+import java.util.List;
+
+import com.myweb.model.MUser;
+
+public interface MUserServiceI {
+
+	List<MUser> getAll();
+	
+	MUser selectByPrimaryKey(String id);
+	
+    int insert(MUser muser);
+    
+    int update(MUser muser);
+    
+    int delete(String id);
+}
